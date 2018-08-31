254f7ac0bec689927da9c0dd1ef12f1838b791aa
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-31 21:36:36 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,7 +2,7 @@ class RepositoriesController < ApplicationController
 
   def index
     resp = Faraday.get("https://api.github.com/user/repos") do |req|
-      req.headers['Authorization'] = 'token' session[:token]
+      req.headers['Authorization'] = 'token ' session[:token]
       req.headers['Accept'] = 'application/json'
     end
     @repos = JSON.parse(resp.body)
